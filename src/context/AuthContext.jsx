c178228d235257f594b1af72b5dac9b31a5f2d30
김민정 <mjkim@ipartners.co.kr>
2023-04-07 17:39:55 +0900

    api firebase 수정

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { login, logout, onUserStateChange } from "api/fisebase";
+
+const { createContext, useContext, useState, useEffect } = require("react");
+const AuthContext = createContext();
+
+export function AuthContextProvider({children}) {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    // callback 함수받는 onUserStateChange
+    onUserStateChange((user) => {
+      setUser(user)
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{user, login, logout}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuthContext(){
+  return useContext(AuthContext);
+}
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,5 +1,9 @@
-import firebase from 'firebase'
+//firebase 초기화
 import { initializeApp } from "firebase/app";
+// import { v4 as uuid } from 'uuid';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, get, set } from "firebase/database";
+import { v4 as uuid } from 'uuid';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -9,7 +13,60 @@ const firebaseConfig = {
 };
 
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+// 재로그인할때 자동로그인 막기
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
 
-export default firebaseApp;
+provider.setCustomParameters({prompt: 'select_account',})
+
+export async function login() {
+  return signInWithPopup(auth, provider)
+  .then((result) => {
+    const user = result.user;
+    return user;
+  }).catch((error) => {
+    console.log('lgoin test');
+    return error.message;
+  });
+}
+
+export async function logout() {
+  return signOut(auth)
+  .then(() => {
+    return null;
+  }).catch((error) => {
+    return error.message;
+  });
+}
+
+// 로그인정보 옵저버
+export async function onUserStateChange( callBack ) {
+  onAuthStateChanged(auth, async (user) => { // 비동기방식이라 async 적용
+    const updatedUser = user ? await user : null;
+    callBack(updatedUser);
+  });
+}
+
+
+// database update
+const database = getDatabase(app);
+
+export async function addNewCard( card ){
+  const id = uuid();
+  const { name, theme, company, title, email, mobile, message, fileName, fileURL } = card
+  
+  return set(ref(database, `card/${id}`), {
+    ...card,
+    id,
+    name,
+    theme,
+    company,
+    title,
+    email,
+    mobile,
+    message,
+    fileName,
+    fileURL,
+  })
+}
