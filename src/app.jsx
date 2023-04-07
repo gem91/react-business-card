eccf7a9daea1541c9d6336e24e58f07c0cf1ddb1
김민정 <mjkim@ipartners.co.kr>
2023-04-07 17:46:11 +0900

    router 개선

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import styles from './app.module.css'
-import Login from './components/login/login';
-import Maker from './components/maker/maker';
+import Login from './components/login';
+import Maker from './components/maker';
 
 function App({ FileInput, authService }) {
+
+  const [user, setUser] = useState();
+
   return (
-    <div className={styles.app}>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/*" element={<Login authService={authService} />}></Route>
-            <Route path="/maker" element={<Maker authService={authService} FileInput={FileInput} />}></Route>
-        </Routes>
-      </BrowserRouter>
-      
-    </div>
+      <div className={styles.app}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Routes>
+              <Route path="/*" element={<Login user={user} setUser={setUser} />}></Route>
+              <Route path="/maker" element={<Maker user={user} setUser={setUser} FileInput={FileInput} />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import AuthService from './service/auth_service';
-import ImageUploader from './service/image_uploader';
-import ImageFileInput from './components/image_input/imageFileInput';
+// import ImageUploader from './api/image_uploader';
+// import ImageFileInput from './components/image_input/imageFileInput';
 
-const authService = new AuthService();
-const imageUploader = new ImageUploader();
+// const imageUploader = new ImageUploader();
 
-const FileInput = (props) => (<ImageFileInput {...props} imageUploader={imageUploader} />) 
+// const FileInput = (props) => (<ImageFileInput {...props} imageUploader={imageUploader} />) 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} FileInput={FileInput} />
+    <App></App>
+    {/* <App FileInput={FileInput} /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
